d307b3e33a8fb700aef0bf9892a54215321f66e9
Memo Chou <memochou1993@gmail.com>
2024-10-13 16:20:33 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    include: [
-      'lib/**/*.test.ts',
-    ],
   },
   resolve: {
     alias: {
