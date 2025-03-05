ab8486d7d35e5af61b826b2a3ed6ed25d1637e23
Memo Chou <memochou1993@gmail.com>
2025-03-05 16:27:38 +0800

    Enhance formatMessage utility to improve spacing around full-width punctuation

--- lib/formatMessage.ts
+++ lib/formatMessage.ts
@@ -1,7 +1,15 @@
 const formatMessage = (value: string) => {
+  const fullWidthMarks = '，、；：！？。（）《》「」『』【】〔〕';
+  const ascii = '\\x20-\\x7E';
+  const noSpaceBefore = new RegExp(`\\s*([${fullWidthMarks}])`, 'gu');
+  const noSpaceAfter = new RegExp(`([${fullWidthMarks}])\\s*([${ascii}])`, 'gu');
+  const addSpaceBefore = new RegExp(`([^${ascii}${fullWidthMarks}])([${ascii}])`, 'gu');
+  const addSpaceAfter = new RegExp(`([${ascii}])([^${ascii}${fullWidthMarks}])`, 'gu');
   return value
-    .replace(/([^\x20-\x7E])([\x20-\x7E])/gu, '$1 $2')
-    .replace(/([\x20-\x7E])([^\x20-\x7E])/gu, '$1 $2')
+    .replace(noSpaceBefore, '$1')
+    .replace(noSpaceAfter, '$1$2')
+    .replace(addSpaceBefore, '$1 $2')
+    .replace(addSpaceAfter, '$1 $2')
     .replace(/ +/g, ' ');
 };
 
